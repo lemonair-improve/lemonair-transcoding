bec9f04faf571e4d493c05c69df540ecb67530d8
munee <121735319+alaneelee@users.noreply.github.com>
2023-12-05 20:59:37 +0900

    comment: TranscodeService 기능 분석 후 코멘트 작성

--- src/main/java/com/hanghae/lemonairtranscoding/service/TranscodeService.java
+++ src/main/java/com/hanghae/lemonairtranscoding/service/TranscodeService.java
@@ -48,14 +48,15 @@ public class TranscodeService {
 
 
 	// 동시성 떄문에 사용
-	private final ConcurrentHashMap<String, Process> processMap = new ConcurrentHashMap<>();
-	private static final long delete_interval = 1L;
-	private final ScheduledExecutorService deleteFile = Executors.newSingleThreadScheduledExecutor();
-	private final AtomicBoolean stopSearching = new AtomicBoolean(true);
-	private final AtomicBoolean showMessage = new AtomicBoolean(true);
+	private final ConcurrentHashMap<String, Process> processMap = new ConcurrentHashMap<>(); // 스트림에 대한 FFmpeg 프로세스 관리
+	private static final long delete_interval = 1L; // 파일을 삭제하기 위한 간격 설정
+	private final ScheduledExecutorService deleteFile = Executors.newSingleThreadScheduledExecutor(); // 파일 삭제 작업 스케줄 관리
+	private final AtomicBoolean stopSearching = new AtomicBoolean(true); // 상태 추적 변수
+	private final AtomicBoolean showMessage = new AtomicBoolean(true); // 상태 추적 변수
 
 
 
+	// 리액티브 프로그래밍을 위해 사용
 	private Flux<Path> walkFilesFlux(Path path) {
 		try {
 			return Flux.fromStream(Files.walk(path)
@@ -74,47 +75,47 @@ public class TranscodeService {
 			return;
 		}
 
-		Path directoryPath = Paths.get(outputPath);
+		Path directoryPath = Paths.get(outputPath); // 경로에 대한 Path 객체 생성
 
 		try {
-			Flux<Path> dirPaths = Flux.fromStream(Files.list(directoryPath));
-			dirPaths.filter(Files::isDirectory)
-				.filter(dirPath -> dirPath.toFile().getName().equals(owner))
-				.flatMap(dirPath -> {
-					List<Path> filesToDelete = new ArrayList<>();
-					return walkFilesFlux(dirPath)
-						.doOnNext(file -> {
+			Flux<Path> dirPaths = Flux.fromStream(Files.list(directoryPath)); // directoryPath로부터 디렉토리에 있는 파일과 디렉토리를 스트림으로 받아오는 과정
+			dirPaths.filter(Files::isDirectory) // 받아온 파일과 디렉토리 중 디렉토리만 불러오는 과정
+				.filter(dirPath -> dirPath.toFile().getName().equals(owner)) // 디렉토리 이름이 owner와 같은 것만 추출
+				.flatMap(dirPath -> { // 각 디렉토리에 대해 비동기적으로 작업 수행
+					List<Path> filesToDelete = new ArrayList<>(); // 삭제할 목록을 저장하기 위한 리스트 생성
+					return walkFilesFlux(dirPath) // 디렉토리 내의 파일에 대한 Flux 반환
+						.doOnNext(file -> { // flux에 포함된 파일에 대해 수행할 작업 정의
 							try {
-								BasicFileAttributes attributes = Files.readAttributes(file, BasicFileAttributes.class);
-								Instant currentInstant = Instant.now();
-								Instant fileCreationInstant = attributes.creationTime().toInstant();
-								final long elapsedTime = Duration.between(fileCreationInstant, currentInstant).toMinutes();
+								BasicFileAttributes attributes = Files.readAttributes(file, BasicFileAttributes.class); // 파일의 속성을 읽어옴
+								Instant currentInstant = Instant.now(); // 현재 시간을 얻어와서 Instant 객체로 저장
+								Instant fileCreationInstant = attributes.creationTime().toInstant(); // 파일 생성 시간을 가져와 객체로 저장
+								final long elapsedTime = Duration.between(fileCreationInstant, currentInstant).toMinutes(); // 두 시간의 차이를 계산하고 분단위로 저장
 
-								if (elapsedTime >= 1) {
+								if (elapsedTime >= 1) { // 1분 이상 경과한 파일들을 filesToDelete 리스트에 저장
 									filesToDelete.add(file);
 								}
 							} catch (IOException e) {
 								log.error("Failed to read attributes of file {}", file, e);
 							}
 						})
-						.doOnComplete(() -> {
+						.doOnComplete(() -> { // Flux의 모든 요소에 대한 처리가 끝났을 때 실행할 작업 정의
 							for (Path file : filesToDelete) {
-								AtomicBoolean hasFiles = new AtomicBoolean(false);
+								AtomicBoolean hasFiles = new AtomicBoolean(false); // 파일이 삭제되었는지 추적하는 데 사용할 변수 초기화
 								try {
-									if (file.toString().endsWith(".ts") || file.toString().endsWith(".jpg")) {
+									if (file.toString().endsWith(".ts") || file.toString().endsWith(".jpg")) { // .ts와 .jpg 파일 삭제
 										Files.deleteIfExists(file);
-										hasFiles.set(true);
+										hasFiles.set(true); // 파일 삭제 추적값 true
 									}
 								} catch (IOException e) {
 									log.error("Failed to delete file {}", file, e);
 								}
 								if (!hasFiles.get()) {
-									stopSearching.set(true);
+									stopSearching.set(true); // 만약 삭제된 파일이 없었다면 stopSearching을 true로 하여 파일 탐색을 중지
 								}
 							}
 						})
-						.then()
-						.subscribeOn(Schedulers.boundedElastic());
+						.then()// 반환할 Mono 생성 (아무 값도 반환하지 않음)
+						.subscribeOn(Schedulers.boundedElastic()); // 비동기적으로 실행되도록 스케줄러 지정(별도의 스레드에서 실행하도록)
 				})
 				.subscribe();
 		} catch (IOException e) {
@@ -157,9 +158,9 @@ public class TranscodeService {
 
 	public Mono<Long> start(String owner){
 		log.info("streaming server 에서 transcoding server에 접근");
-		deleteFile.scheduleAtFixedRate(() -> this.deleteOldTsAndJpgFiles(owner), delete_interval, delete_interval, TimeUnit.MINUTES);
+		deleteFile.scheduleAtFixedRate(() -> this.deleteOldTsAndJpgFiles(owner), delete_interval, delete_interval, TimeUnit.MINUTES); // deleteOldTsAndJpgFiles 메서드를 일정한 간격으로 호출
 		// isAlive() - 하위 프로세스가 Process활성 상태인지 테스트
-		if(processMap.containsKey(owner) && processMap.get(owner).isAlive()){
+		if(processMap.containsKey(owner) && processMap.get(owner).isAlive()){ // 현재 작업중인 프로세스가 있는지 확인 만약 owner에 해당하는 프로세스가 있고 활성화 되어있다면 프로세스 종료
 			// 하위 프로세스를 종료
 			processMap.get(owner).destroyForcibly();
 			processMap.remove(owner);
@@ -219,8 +220,8 @@ public class TranscodeService {
 
 		// end of Input 에러
 		return Mono
-			.fromCallable(() -> {
-				Path directory = Paths.get(outputPath).resolve(owner);
+			.fromCallable(() -> { // Callable 객체로부터 Mono 생성
+				Path directory = Paths.get(outputPath).resolve(owner); // outputPath와 owner를 합쳐 경로 생성
 				// 경로가 폴더인지 확인
 				if (!Files.exists(Paths.get(outputPath))) {
 					try {
@@ -247,7 +248,7 @@ public class TranscodeService {
 				log.info(process.info().toString());
 				log.info(String.valueOf(process.pid()));
 				// onExit() - 프로세스 종료를 위한 CompletableFuture<Process>를 반환
-				process.onExit().thenAccept((c) -> {
+				process.onExit().thenAccept((c) -> { // 프로세스의 종료를 감지하고 해당 프로세스가 종료되면 실행될 작업들 정의
 					log.info(owner + " exited with code " + c.exitValue());
 					if (!processMap.get(owner).isAlive()) {
 						processMap.remove(owner);
@@ -260,7 +261,7 @@ public class TranscodeService {
 				});
 				processMap.put(owner, process);
 				// 프로세스의 기본 프로세스 ID를 반환합니다. 기본 프로세스 ID는 운영 체제가 프로세스에 할당하는 식별 번호
-				return Mono.just(process.pid());
+				return Mono.just(process.pid()); // 정상적으로 종료되지 않은 process.pid 반환
 				// 블로킹 IO 태스크와 같은 생명주기가 긴 태스크들에 적합하다.
 				// boundedElastic 은 요청 할때마다 스레드 생성 단, 스레드 수 제한
 			}).subscribeOn(Schedulers.boundedElastic());

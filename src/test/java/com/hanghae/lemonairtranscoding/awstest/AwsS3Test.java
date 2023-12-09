3fc7c83fb0c8b914ebca1f714c83f69fbfbb86d3
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-09 21:32:52 +0900

    aws upload 비동기로 적용했으나 upload 예외 발생하는 상황

--- src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
+++ src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
 import com.hanghae.lemonairtranscoding.aws.AwsS3Uploader;
 
 
@@ -23,5 +24,17 @@ public class AwsS3Test {
 	void uploadFileTest(){
 		File file = new File("C:\\Users\\sbl\\Desktop\\uploadtest.txt.txt");
 		amazonS3.putObject("lemonair-streaming", "sbl/sm2", file);
+	}
+
+	@Test
+	void checkFiles(){
+		S3Object s3Object = amazonS3.getObject("lemonair-streaming", "sbl/sm2");
+		System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
+	}
+
+	@Test
+	void checkVidoe(){
+		S3Object s3Object = amazonS3.getObject("lemonair-streaming", "lyulbyung/videos/lyulbyung.m3u8");
+		System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
 	}
 }

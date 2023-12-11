6e2ab5b1efa2979bc331996b9fb72ab0f1b7c94b
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-11 13:07:00 +0900

    TranscodeService.java에서 모든 로직 수행 및 필요없는 파일 삭제

--- src/main/java/com/hanghae/lemonairtranscoding/LemonairTranscodingApplication.java
+++ src/main/java/com/hanghae/lemonairtranscoding/LemonairTranscodingApplication.java
@@ -3,8 +3,6 @@ package com.hanghae.lemonairtranscoding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
 public class LemonairTranscodingApplication {
 	public static void main(String[] args) {

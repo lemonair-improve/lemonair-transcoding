5e04cf2bb88454ef2fcea146633cf16d6a69ad91
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-20 02:17:10 +0900

    AwsService에서 m3u8파일을 업로드할때는 no-cache metadata설정하여 업로드하도록 수정

--- src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
+++ src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
@@ -1,5 +1,6 @@
 package com.hanghae.lemonairtranscoding.awstest;
 
+import java.awt.*;
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 
 
@@ -40,5 +43,13 @@ public class AwsS3Test {
 	void checkVidoe(){
 		S3Object s3Object = amazonS3.getObject("lemonair-streaming", "lyulbyung/videos/lyulbyung.m3u8");
 		System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
+	}
+
+	@Test
+	void uploadFileWithNoCacheMetadata(){
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setCacheControl("max-age=0, no-cache, no-store, must-revalidate");
+		PutObjectRequest putObjectRequest = new PutObjectRequest("lemonair-streaming", "test.txt", new File("C:/Users/sbl/Desktop/test.txt")).withMetadata(metadata);
+		amazonS3.putObject(putObjectRequest);
 	}
 }

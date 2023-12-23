c705f01fbec3a36c7dc1876caac735e8dc24d9a9
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-23 16:53:55 +0900

    Aws 테스트는 주석, 다른 학습테스트들은 lemonair-learntest로 이전

--- src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
+++ src/test/java/com/hanghae/lemonairtranscoding/awstest/AwsS3Test.java
@@ -26,65 +26,65 @@ public class AwsS3Test {
 	@Value("${aws.s3.bucket}")
 	private String bucket;
 
+	// // @Test
+	// // void uploadFileSyncTest() {
+	// // 	File file = new File("C:\\Users\\sbl\\Desktop\\uploadtest.txt.txt");
+	// // 	s3SyncClient.putObject(bucket, "sbl/sm2", file);
+	// // }
+	// //
+	// // @Test
+	// // void uploadFileSyncTest2() {
+	// // 	File file = new File("C:\\Users\\sbl\\Desktop\\uploadtest.txt.txt");
+	// // 	s3SyncClient.putObject(bucket, "lyulbyung/videos", file);
+	// // }
+	//
+	// /**
+	//  * cache-control 값은 PutObjectRequest.Builder에서 build 메서드를 제공하고있기 때문에
+	//  * metadata로 직접 지정하지 않아도 된다.
+	//  */
 	// @Test
-	// void uploadFileSyncTest() {
-	// 	File file = new File("C:\\Users\\sbl\\Desktop\\uploadtest.txt.txt");
-	// 	s3SyncClient.putObject(bucket, "sbl/sm2", file);
+	// void uploadFileAsyncTest() {
+	// 	String fileName = "uploadtest.txt";
+	// 	PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+	// 		.key("uploadtest/" + fileName)
+	// 		.bucket(bucket)
+	// 		.cacheControl("max-age=0, no-cache, no-store, must-revalidate")
+	// 		.build();
+	// 	Path path = Path.of("C:\\Users\\sbl\\Desktop\\" + fileName);
+	// 	s3AsyncClient.putObject(putObjectRequest, path).join();
 	// }
 	//
 	// @Test
-	// void uploadFileSyncTest2() {
-	// 	File file = new File("C:\\Users\\sbl\\Desktop\\uploadtest.txt.txt");
-	// 	s3SyncClient.putObject(bucket, "lyulbyung/videos", file);
+	// void getObjectMetadataTest() {
+	// 	ObjectMetadata metadata = s3SyncClient.getObjectMetadata(bucket, "uploadtest/uploadtest.txt");
+	// 	// 표준 메타데이터 출력
+	// 	System.out.println("Cache-Control: " + metadata.getCacheControl());
+	// 	System.out.println("Content-Disposition: " + metadata.getContentDisposition());
+	// 	System.out.println("Content-Encoding: " + metadata.getContentEncoding());
+	// 	System.out.println("Content-Length: " + metadata.getContentLength());
+	// 	System.out.println("Content-Type: " + metadata.getContentType());
+	// 	System.out.println("ETag: " + metadata.getETag());
+	// 	System.out.println(metadata.getUserMetadata());
 	// }
-
-	/**
-	 * cache-control 값은 PutObjectRequest.Builder에서 build 메서드를 제공하고있기 때문에
-	 * metadata로 직접 지정하지 않아도 된다.
-	 */
-	@Test
-	void uploadFileAsyncTest() {
-		String fileName = "uploadtest.txt";
-		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-			.key("uploadtest/" + fileName)
-			.bucket(bucket)
-			.cacheControl("max-age=0, no-cache, no-store, must-revalidate")
-			.build();
-		Path path = Path.of("C:\\Users\\sbl\\Desktop\\" + fileName);
-		s3AsyncClient.putObject(putObjectRequest, path).join();
-	}
-
-	@Test
-	void getObjectMetadataTest() {
-		ObjectMetadata metadata = s3SyncClient.getObjectMetadata(bucket, "uploadtest/uploadtest.txt");
-		// 표준 메타데이터 출력
-		System.out.println("Cache-Control: " + metadata.getCacheControl());
-		System.out.println("Content-Disposition: " + metadata.getContentDisposition());
-		System.out.println("Content-Encoding: " + metadata.getContentEncoding());
-		System.out.println("Content-Length: " + metadata.getContentLength());
-		System.out.println("Content-Type: " + metadata.getContentType());
-		System.out.println("ETag: " + metadata.getETag());
-		System.out.println(metadata.getUserMetadata());
-	}
-
+	//
+	// // @Test
+	// // void checkFiles() {
+	// // 	S3Object s3Object = s3SyncClient.getObject(bucket, "sbl/sm2");
+	// // 	System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
+	// // }
+	//
+	// // @Test
+	// // void checkVideo() {
+	// // 	S3Object s3Object = s3SyncClient.getObject(bucket, "lyulbyung/videos/lyulbyung.m3u8");
+	// // 	System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
+	// // }
+	//
 	// @Test
-	// void checkFiles() {
-	// 	S3Object s3Object = s3SyncClient.getObject(bucket, "sbl/sm2");
-	// 	System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
+	// void uploadFileWithNoCacheMetadata() {
+	// 	ObjectMetadata metadata = new ObjectMetadata();
+	// 	metadata.setCacheControl("max-age=0, no-cache, no-store, must-revalidate");
+	// 	// PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, "test.txt",
+	// 	// 	new File("C:/Users/sbl/Desktop/test.txt")).withMetadata(metadata);
+	// 	// amazonS3.putObject(putObjectRequest);
 	// }
-
-	// @Test
-	// void checkVideo() {
-	// 	S3Object s3Object = s3SyncClient.getObject(bucket, "lyulbyung/videos/lyulbyung.m3u8");
-	// 	System.out.println("s3Object.getObjectContent().toString() = " + s3Object.getObjectContent().toString());
-	// }
-
-	@Test
-	void uploadFileWithNoCacheMetadata() {
-		ObjectMetadata metadata = new ObjectMetadata();
-		metadata.setCacheControl("max-age=0, no-cache, no-store, must-revalidate");
-		// PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, "test.txt",
-		// 	new File("C:/Users/sbl/Desktop/test.txt")).withMetadata(metadata);
-		// amazonS3.putObject(putObjectRequest);
-	}
 }

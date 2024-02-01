950ca60417e7bb3f3dec1f2e8a95e98ef7bbdb15
BYEONGRYEOL <lyulbyung@gmail.com>
2024-02-01 18:25:04 +0900

    Feat : 영상 송출 테스트 스크립트
    Ref #2

---
+++ ffmpeg/stream-test-video-test20.bat
@@ -0,0 +1,2 @@
+@echo off
+ffmpeg -re -i testvideo.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -strict experimental -f flv rtmp://localhost:1935/test20/b66d404b-5035-4b49-8b38-70238c0ea1f9
